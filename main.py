a4534aa3bbfa1839df506d0796102bfad388a0a1
Evanderss <evanderramirez150@gmail.com>
2022-09-27 00:18:02 -0500

    Crando la función update para modificar la data de los clientes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-"""var clients saves name of clients"""
 clients = "David, Angeles, "
+"""var clients saves name of clients"""
 
 
-"""this function add the name clients with the capital letter"""
 def create_client(client_name):
+    """this function add the name clients with the capital letter"""
     global clients
     if client_name not in clients:
         clients += client_name.capitalize()
@@ -11,29 +11,50 @@ def create_client(client_name):
     else:
         print("The client is already in the clients list")
 
-"""this function shows the clients list"""
+
 def list_clients():
+    """this function shows the clients list"""
     global clients
     print(clients)
+
+
+def update_client(client_name, updated_client_name):
+    """this function update data about clients"""
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", updated_client_name + ",")
+    else:
+        print("Client is not in the clients list")
+
 
 def _add_comma():
     global clients
     clients += ", "
 
-"""elcome message to clients"""
+
 def _print_welcome():
+    """welcome message to clients"""
     print("Welcome to my store page:")
-    print("*" * 50)
+    print("*" * 70)
     print("What would you like to do today?")
     print("[C]reate")
+    print("[U]pdate")
     print("[D]elete")
 
+
+def _get_client_name():
+    return input("What is the client name? ")
 if __name__ == '__main__':
     _print_welcome()
     command = input().upper()
     if command == "C":
-        client_name = input("What is the client name?")
+        client_name = _get_client_name()
         create_client(client_name)
+        list_clients()
+    elif command == "U":
+        client_name = _get_client_name()
+        updated_client_name = input("What is the updated client name? ")
+        update_client(client_name, updated_client_name)
         list_clients()
     elif command == "D":
         pass
