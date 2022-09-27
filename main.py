35ea6e937a20187a4d17ae91cd023cf52fae23d8
Evanderss <evanderramirez150@gmail.com>
2022-09-27 16:04:55 -0500

    Usando while loops para evitar errores por parte del cliente, además de exit para romper el ciclo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 clients = "David, Angeles, "
 """var clients saves name of clients"""
 
@@ -62,7 +63,20 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input("What is the client name? ")
+    client_name = None
+    while not client_name:
+        client_name = input("What is the client name? ")
+
+        if client_name == "exit":
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
+
+
 if __name__ == '__main__':
     _print_welcome()
     command = input().upper()
