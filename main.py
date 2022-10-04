0cb6e677bf70b42e5072bec4c062dcdfcbae942e
Evanderss <evanderramirez150@gmail.com>
2022-10-04 08:50:48 -0500

    Agregando [read] al crud y aplicando dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-from unicodedata import name
 clients = [
     {
         "name": "David",
@@ -34,41 +33,31 @@ def list_clients():
         print(f"{uid} | {name} | {company} | {position}")
             
 
-def update_client(client_name, updated_client_name):
+def update_client(client_id, updated_client_name):
     """this function update data about clients"""
     global clients
-    if client_name in clients:
-        index = clients.index(client_name)
-        clients[index] = updated_client_name
+    if len(clients) - 1 >= client_id:
+        clients[client_id] = updated_client_name
     else:
         print("Client is not in the clients list")
 
 
-def delete_client(client_name):
+def delete_client(client_id):
     global clients
-    if client_name in clients:
-        clients.remove(client_name)
-    else: 
-        print("Client is not in the clients list")
+    for idx, client in enumerate(clients):
+        if idx == client_id:
+            del clients[idx] 
+            break
+        else: 
+            print("Client is not in the clients list")
 
 
 def search_client(client_name):
     for client in clients:
-        if client != client_name:
+        if client["name"] != client_name:
             continue
         else:
             return True
-
-
-def _print_welcome():
-    """welcome message to clients"""
-    print("Welcome to my store page:")
-    print("*" * 100)
-    print("What would you like to do today?")
-    print("[C]reate")
-    print("[U]pdate")
-    print("[D]elete")
-    print("[S]earch")
 
 
 def _get_client_field(field_name):
@@ -81,32 +70,47 @@ def _get_client_name():
     client_name = None
     while not client_name:
         client_name = input("What is the client name? ")
-
         if client_name == "exit":
             client_name = None
             break
-
     if not client_name:
         sys.exit()
-
     return client_name
+
+
+def _get_client_from_user():
+    client = {
+        'name': _get_client_field('name'),
+        'company': _get_client_field('company'),
+        'position': _get_client_field('position'),}
+    return client
+
+
+def _print_welcome():
+    """welcome message to clients"""
+    print("Welcome to my store page:")
+    print("*" * 100)
+    print("What would you like to do today?")
+    print("[C]reate client")
+    print("[R]ead list of clients")
+    print("[U]pdate client")
+    print("[D]elete client")
+    print("[S]earch client")
 
 
 if __name__ == '__main__':
     _print_welcome()
     command = input().upper()
     if command == "C":
-        client = {
-            "name": _get_client_field("name"),
-            "company": _get_client_field("company"),
-            "position": _get_client_field("position"),
-        }
+        client = _get_client_from_user()
         create_client(client)
         list_clients()
+    elif command == "R":
+        list_clients()
     elif command == "U":
-        client_name = _get_client_name()
-        updated_client_name = input("What is the updated client name? ")
-        update_client(client_name, updated_client_name)
+        client_id = int(_get_client_field('id'))
+        updated_client = _get_client_from_user()
+        update_client(client_id, updated_client)
         list_clients()
     elif command == "D":
         client_name = _get_client_name()
@@ -115,7 +119,6 @@ if __name__ == '__main__':
     elif command == "S":
         client_name = _get_client_name()
         found = search_client(client_name)
-
         if found:
             print("The client is in the clients list")
         else:
